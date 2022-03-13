41e893eb3de848d02cd93a55aef7572798d6d08b
Robin Glen <robinglen@gmail.com>
2022-03-13 14:04:07 +0000

    starting adding in tests

--- src/demo/fastify.js
+++ src/demo/fastify.js
@@ -1,11 +1,8 @@
 import Fastify from "fastify";
-// including to see if we can handle if already added
-import fastifyForm from "fastify-formbody";
 import { fastWorms } from "../../index.js";
 const app = Fastify();
 const port = 3000;
 
-app.register(fastifyForm);
 app.register(fastWorms, {
   path: "https://raw.githubusercontent.com/thearegee/bookworms/main/demo/config/bookmarks.yaml",
 });
